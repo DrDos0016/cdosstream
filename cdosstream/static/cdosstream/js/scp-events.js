fb914617da5aea691e91b4ce337f06161ee53cc0
Dr. Dos <doctordos@gmail.com>
2024-09-04 19:55:03 -0700

    Properly fix layout for streaming PC

--- cdosstream/static/cdosstream/js/scp-events.js
+++ cdosstream/static/cdosstream/js/scp-events.js
@@ -7,35 +7,74 @@ function stream_info(event)
 
 function use_the_3d_talk_engine(event)
 {
-    var time = event.meta.created_at.slice(11,19);
+    var time = event.meta.created_at.slice(14,19);
     var username = username = event.body.event.user_name;
-    var row = `<tr class="row-${event.meta.kind}" data-pk="${event.meta.pk}"><td class="event-pk">${event.meta.pk}</td><td class="event-time">${time}</td><td class="event-kind">${event.meta.kind}</td><td class="event-user">${username}</tr>`;
-    var row2 = `\n<tr class="row-${event.meta.kind} extra-row"><td colspan='4'>&lt;${username}&gt; ${event.body.event.user_input}</tr>`;
-    $("#event-overview").prepend(row + row2);
+    
+    let output = `
+		<div class="event" data-pk="${event.meta.pk}">
+			<div class="event-row">
+				<div class="event-pk">${event.meta.pk}</div>
+				<div class="event-time">${time}</div>
+				<div class="event-kind">${event.meta.kind}</div>
+				<div class="event-user">${username}</div>
+			</div>
+			<div class="event-row">
+				<div class="event-message">&lt;${username}&gt; ${event.body.event.user_input}</div>
+			</div>
+		</div>
+    `;
+    
+    $("#event-overview").prepend(output);
 }
 
 function random_scroll(event)
 {
-    var time = event.meta.created_at.slice(11,19);
-    var username = username = event.body.event.user_name;
-    var row = `<tr class="row-${event.meta.kind}" data-pk="${event.meta.pk}"><td class="event-pk">${event.meta.pk}</td><td class="event-time">${time}</td><td class="event-kind">${event.meta.kind}</td><td class="event-user">${username}</tr>`;
-    $("#event-overview").prepend(row);
+    var time = event.meta.created_at.slice(14,19);
+    var username = event.body.event.user_name;
+    
+    let output = `
+		<div class="event" data-pk="${event.meta.pk}">
+			<div class="event-row">
+				<div class="event-pk">${event.meta.pk}</div>
+				<div class="event-time">${time}</div>
+				<div class="event-kind">${event.meta.kind}</div>
+				<div class="event-user">${username}</div>
+			</div>
+		</div>
+    `;
+    
+    
+    $("#event-overview").prepend(output);
 }
 
 function default_log(event)
 {
     var row2 = ""
     var username = "";
-    var time = event.meta.created_at.slice(11,19);
+    var time = event.meta.created_at.slice(14,19);
+    let extra_row = "";
 
     if (event.body.event.user_name)
-        username = event.body.event.user_name;
-
-    var row = `<tr class="row-${event.meta.kind}" data-pk="${event.meta.pk}"><td class="event-pk">${event.meta.pk}</td><td class="event-time">${time}</td><td class="event-kind">${event.meta.kind}</td><td class="event-user">${username}</tr>`;
+        username = event.body.event.user_name;	
 
     if (event.meta.kind == "channelsubscriptionmessage")
     {
-        var row2 = `\n<tr class="row-${event.meta.kind} extra-row"><td colspan='4'>&lt;${username}&gt; ${event.body.event.message.text}</tr>`;
+         extra_row = `<div class="event-row highlight">
+			<div class="event-message">&lt;${username}&gt; ${event.body.event.message.text}</div>
+         </div>`;
     }
-    $("#event-overview").prepend(row + row2);
+    
+    let output = `
+		<div class="event" data-pk="${event.meta.pk}">
+			<div class="event-row">
+				<div class="event-pk">${event.meta.pk}</div>
+				<div class="event-time">${time}</div>
+				<div class="event-kind">${event.meta.kind}</div>
+				<div class="event-user">${username}</div>
+			</div>
+			${extra_row}
+		</div>
+    `;
+    
+    $("#event-overview").prepend(output);
 }
