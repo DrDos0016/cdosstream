a0fade997dc4802d5280339816b7e0ed28d4087f
Dr. Dos <doctordos@gmail.com>
2024-09-06 15:34:34 -0700

    Recalibrate volume

--- cdosstream/static/cdosstream/js/modules/events.js
+++ cdosstream/static/cdosstream/js/modules/events.js
@@ -1,9 +1,11 @@
 var speed = {"event_fade": 250};
 
+// Audio volume should be between -10 and -15db w/ 100% slider volume in OBS
+
 export async function bip_bo_beep(event)  /* Ref: #69 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.15;
+        sound.volume = 0.25;
         for (var idx = 1; idx < 9; idx++)
         {
             await play_sound("/static/cdosstream/event/bip-bo-beep/f-" + (idx % 8 + 1)  + ".mp3");
@@ -16,7 +18,7 @@ export async function bip_bo_beep(event)  /* Ref: #69 */
 export async function channelcheer(event)  /* Ref: #260 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/channelcheer/bits-gem.wav");
         await play_sound("/static/cdosstream/event/channelcheer/bits-gem.wav");
         await play_sound("/static/cdosstream/event/channelcheer/bits-gem.wav");
@@ -29,7 +31,7 @@ export async function channelcheer(event)  /* Ref: #260 */
 export async function channelfollow(event)  /* Ref: #249 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/channelfollow/follow-transporter.wav");
         await delay(3000);
         fade_out_event_card(event);
@@ -40,7 +42,7 @@ export async function channelfollow(event)  /* Ref: #249 */
 export async function channelraid(event)  /* Ref: #1035 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/channelraid/raid-passage.wav");
         await delay(2000);
         fade_out_event_card(event);
@@ -52,7 +54,7 @@ export async function channelsubscribe(event)  /* Ref: #243 for basic sub, #1557
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
         //update_sub_info();
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/channelsubscribe/sub-key.wav");
         await delay(2000);
         fade_out_event_card(event);
@@ -63,7 +65,7 @@ export async function channelsubscriptionmessage(event)  /* Ref: #1550 Resub */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
         //update_sub_info();
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/channelsubscriptionmessage/sub-key.wav");
         await delay(2000);
         fade_out_event_card(event);
@@ -75,7 +77,7 @@ export async function channelsubscriptiongift(event)  /* Ref: 1565 / 1556 Gift S
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
         //update_sub_info();
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/channelsubscriptiongift/sub-key.wav");
         await delay(2000);
         fade_out_event_card(event);
@@ -86,7 +88,7 @@ export async function channelsubscriptiongift(event)  /* Ref: 1565 / 1556 Gift S
 export async function hydrate(event)  /* Ref: #991 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/hydrate/hydrate-water.wav");
         await delay(1000);
         fade_out_event_card(event);
@@ -98,7 +100,7 @@ export async function its_bird_oclock_somewhere(event)  /* Ref: #268 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
         let hour = $("#live-event").data("hour");
-        sound.volume = 0.25;
+        sound.volume = 0.90;
         await play_sound("/static/cdosstream/event/its-bird-oclock-somewhere/bc-" + hour + ".mp3");
         fade_out_event_card(event);
     });
@@ -109,7 +111,7 @@ export async function its_bird_oclock_somewhere(event)  /* Ref: #268 */
 export async function posture_check(event)  /* Ref: #252 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.25;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/posture-check/pushing.wav");
         await delay(2000);
         fade_out_event_card(event);
@@ -119,17 +121,17 @@ export async function posture_check(event)  /* Ref: #252 */
 export async function streeeeeeeeeetch(event)  /* Ref: #159 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.25;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/streeeeeeeeeetch/pushing.wav");
         await delay(2000);
         fade_out_event_card(event);
     });
 }
 
-export async function sub_goal(event)  /* Ref: #??? */
+export async function sub_goal(event)  /* Ref: #3780 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 1;
+        sound.volume = 0.5;
         await play_sound("/static/cdosstream/event/sub-goal/newthingget-normalized.mp3");
         fade_out_event_card(event);
     });
@@ -138,7 +140,7 @@ export async function sub_goal(event)  /* Ref: #??? */
 export async function use_the_3d_talk_engine(event)  /* Ref: #1033 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.15;
+        sound.volume = 0.30;
         await play_sound("/static/cdosstream/event/use-the-3d-talk-engine/3d-talk.wav");
         await delay(800);
         await play_sound("/static/cdosstream/event/use-the-3d-talk-engine/3d-talk.wav");
@@ -150,7 +152,7 @@ export async function use_the_3d_talk_engine(event)  /* Ref: #1033 */
 export async function yeaaaaahh(event)  /* Ref: #1046 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.2;
+        sound.volume = 0.50;
         await play_sound("/static/cdosstream/event/yeaaaaahh/yeah.wav");
         await delay(1600);
         fade_out_event_card(event);
@@ -160,7 +162,7 @@ export async function yeaaaaahh(event)  /* Ref: #1046 */
 export async function zzt_toilet_flush(event)  /* Ref: #973 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
+        sound.volume = 0.40;
         await play_sound("/static/cdosstream/event/zzt-toilet-flush/flush.mp3");
         await delay(2000);
         fade_out_event_card(event);
@@ -170,7 +172,7 @@ export async function zzt_toilet_flush(event)  /* Ref: #973 */
 export async function random_scroll(event)  /* Ref: #2511 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.50;
+        sound.volume = 0.80;
         await play_sound("/static/cdosstream/event/random-scroll/scroll.mp3");
         await delay(12000);
         fade_out_event_card(event);
