81a41d7d8d170068ce3938e4e98a57bb929993b3
Dr. Dos <doctordos@gmail.com>
2025-02-21 14:31:40 -0800

    Websocket connection as base class

--- cdosstream/static/cdosstream/js/modules/websocket_connection.js
+++ cdosstream/static/cdosstream/js/modules/websocket_connection.js
@@ -1,15 +1,29 @@
+export let SHARED_LIST = ["foo", "bar", "baz"];
+
 export class Websocket_Connection
 {
-    constructor(host, port) {
+    constructor(host, port, name="Foobar")
+    {
         this.name = "Websocket Connection";
+        this.connection_icon_selector = "#NOSELECTOR";
+        this.connection_good_icon = "✅";
+        this.connection_bad_icon = "❌";
+        this.address = "";
+        this.groups = [];
+
+        this.needs_identification = true;
         this.uuid = "";
+
         this.websocket = null;
         this.host = host;
         this.port = port;
+        this.log_all_events = true;
     }
 
     init()
     {
+        this.setup();
+        console.log("Init", this.name);
         if (this.websocket)
             return false;
 
@@ -17,6 +31,21 @@ export class Websocket_Connection
         this.websocket = new WebSocket(`ws://${this.host}:${this.port}/`);
         this.websocket.addEventListener("message", (event) => this.ws_receive(event));
         this.websocket.addEventListener("error", (event) => this.ws_close(event));
+    }
+
+    identify_connection(event)
+    {
+        let message = event.data;
+        console.log(message);
+        if (! this.uuid)
+        {
+            this.uuid = message;
+            console.log(`${this.name} identify connection func [UUID=${this.uuid}]`);
+            this.connection_established();
+            this.ws_send({"command": "identify-connection"});
+            return true;
+        }
+        return false;
     }
 
     toggle_connection()
@@ -29,17 +58,11 @@ export class Websocket_Connection
 
     ws_receive(event)
     {
-        console.log("CALLED RECEIVE WITH EVENT", event);
-        let message = event.data;
-
-        if (! this.uuid)
+        if (this.needs_identification && ! this.uuid)
         {
-            this.uuid = message;
-            this.connection_established();
-            this.ws_send({"command": "identify-connection"});
-            return true;
+            return this.identify_connection(event);
         }
-
+        let message = event.data;
         this.delegate_event(message);
     }
 
@@ -59,8 +82,30 @@ export class Websocket_Connection
         this.connection_lost();
     }
 
-    /* Functions intended to be overridden in subclass */
-    connection_established() { return true; }
-    connection_lost() { return true; }
+    get_timestamp()
+    {
+        let now = new Date();
+        let h = now.getHours();
+        let m = now.getMinutes();
+        let s = now.getSeconds();
+        return `[${h}:${m}:${s}]`;
+    }
+
+    connection_established()
+    {
+        $(this.connection_icon_selector).attr("title", `${this.address} connected as ${this.uuid}`);
+        $(this.connection_icon_selector).html(this.connection_good_icon);
+    }
+
+    connection_lost()
+    {
+        console.log("Lost");
+        $(this.connection_icon_selector).attr("title", `{this.address} has no connection for ${this.uuid}`);
+        $(this.connection_icon_selector).html(this.connection_bad_icon);
+    }
+
+    /* Functions intended to be defined in subclasses */
+    setup() { return true; }
     delegate_event(event) { return true; }
+    // Identify connection may need adjustment as well ^
 }
