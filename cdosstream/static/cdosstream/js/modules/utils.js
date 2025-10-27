ea583d3ed278f0e98d2be651f571a4cf97f25d34
Dr. Dos <doctordos@gmail.com>
2025-10-27 12:40:28 -0700

    Function to take an event and return a JS class based on its key.

--- cdosstream/static/cdosstream/js/modules/utils.js
+++ cdosstream/static/cdosstream/js/modules/utils.js
@@ -9,3 +9,12 @@ export function print_registered_events(registered_events)
     }
     console.log(event_str);
 }
+
+export function raw_event_to_class(event, registered_events)
+{
+    let components = event.meta.kind.split("-");
+    let class_name = "Event_" + components.map((x) => x.slice(0, 1).toUpperCase() + x.slice(1)).join("_");
+    console.log("CONSTRUCTING CLASS: ", class_name);
+    let obj = new registered_events[class_name](event);
+    return obj;
+}
