c782d058ab93e31c283f7dc8788db8407cf8ff6d
Dr. Dos <doctordos@gmail.com>
2025-10-31 17:10:22 -0700

    Convert most events to class format

--- cdosstream/static/cdosstream/js/modules/events.js
+++ cdosstream/static/cdosstream/js/modules/events.js
@@ -5,10 +5,9 @@ var scroll_elapsed_ticks = 0;
 
 export class Redeem_Event_Base {
     constructor(event) {
-        console.log("Base constructor");
         this.class_based_event = true; // Stopgap while classes and functions for events both exist
         this.event = event;
-        this.event_key = "";
+        this.event_key = event.meta.kind.replaceAll("-", "_").toLowerCase();
         this.event_icon = {"fg": "ega-white", "bg": "", "char": "?"};
         this.event_target = "#live-event";
         this.volume = 0.30;
@@ -56,26 +55,12 @@ export class Redeem_Event_Base {
     }
 }
 
-export class Event_Happy_Zzt_Day extends Redeem_Event_Base
+export class Event_Bip_Bo_Beep extends Redeem_Event_Base // Ref 69
 {
     constructor(event)
     {
-        console.log("HZZT Day Constructor");
-        super(event);
-        this.event_key = "happy_zzt_day";
-        this.event_icon = {"fg": "ega-white", "bg": "ega-darkblue", "char": "X"};
-        this.sound_filename = "foo.wav";
-    }
-}
-
-export class Event_Bip_Bo_Beep extends Redeem_Event_Base
-{
-    constructor(event)
-    {
-        console.log("Bip-Bo Constructor");
         super(event);
         this.volume = 0.25;
-        this.event_key = "bip_bo_beep";
         this.event_icon = {"fg": "ega-green", "bg": "", "char": "Z"};
     }
     
@@ -93,215 +78,238 @@ export class Event_Bip_Bo_Beep extends Redeem_Event_Base
     }
 }
 
-export class Event_Beautiful_Music extends Redeem_Event_Base
+export class Event_Beautiful_Music extends Redeem_Event_Base // Ref 5992
 {
     constructor(event)
     {
-        console.log("Beautiful Music Constructor");
         super(event);
-        this.delay = 4000;
-        this.event_key = "beautiful_music";
+        this.delay = 4500;
         this.event_icon = {"fg": "ega-darkred", "bg": "", "char": "☻"};
     }
 }
 
+export class Event_Channelcheer extends Redeem_Event_Base  // Ref 260
+{
+    constructor(event)
+    {
+        super(event);
+        this.event_icon = {"fg": "ega-green", "bg": "", "char": "♦"};
+        this.sound_filename = "bits-gem.wav";
+    }
+    
+    async play()  // Animate the event
+    {
+        $(this.event_target).animate({opacity: 1}, speed.event_fade, async value => {
+            sound.volume = this.volume;
+            await play_sound(this.get_static_path() + "/" + this.sound_filename);
+            await play_sound(this.get_static_path() + "/" + this.sound_filename);
+            await play_sound(this.get_static_path() + "/" + this.sound_filename);
+            await delay(2000);
+            fade_out_event_card(this.event);
+        });
+    }
+}
+
+export class Event_Channelfollow extends Redeem_Event_Base // Ref 249
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 3000;
+        this.event_icon = {"fg": "ega-white", "bg": "ega-darkblue-bg", "char": "☻"};
+        this.sound_filename = "follow-transporter.wav";
+    }
+}
+
+export class Event_Channelraid extends Redeem_Event_Base // Ref 1035
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 3000;
+        this.event_icon = {"fg": "ega-white", "bg": "ega-darkyellow-bg", "char": "≡"};
+        this.sound_filename = "raid-passage.wav";
+    }
+}
+
+export class Event_Channelsubscribe extends Redeem_Event_Base // Ref 243 (basic) / 1557 (gifted)
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 2000;
+        this.event_icon = {"fg": "ega-purple", "bg": "", "char": "♀"};
+        this.sound_filename = "sub-key.wav";
+    }
+}
+
+export class Event_Channelsubscriptionmessage extends Redeem_Event_Base // Ref 1550
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 2000;
+        this.event_icon = {"fg": "ega-purple", "bg": "", "char": "♀"};
+        this.sound_filename = "sub-key.wav";
+    }
+}
+
+export class Event_Channelsubscriptiongift extends Redeem_Event_Base // Ref 1565 (one gift) / 1556 (two gifts)
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 2000;
+        this.event_icon = {"fg": "ega-purple", "bg": "", "char": "♀"};
+        this.sound_filename = "sub-key.wav";
+    }
+}
+
+export class Event_Guide_The_Raid extends Redeem_Event_Base // Ref 5598
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 1600;
+        this.event_icon = {"fg": "ega-green", "bg": "", "char": "►"};
+        this.sound_filename = "raid-passage.wav";
+    }
+}
+
+export class Event_Hahaha extends Redeem_Event_Base // Ref 5877
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 1000;
+        this.event_icon = {"fg": "ega-darkgray", "bg": "", "char": "\""};
+        this.sound_filename = "hahaha.wav";
+    }
+}
+
+export class Event_Happy_Zzt_Day extends Redeem_Event_Base // Ref 5996
+{
+    constructor(event)
+    {
+        super(event);
+        this.event_icon = {"fg": "ega-white", "bg": "ega-darkblue", "char": "X"};
+        this.sound_filename = "foo.wav";
+    }
+}
+
+export class Event_Hydrate extends Redeem_Event_Base // Ref 991
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 1000;
+        this.event_icon = {"fg": "ega-white", "bg": "ega-darkblue-bg", "char": "░"};
+        this.sound_filename = "hydrate-water.wav";
+    }
+}
+
+export class Event_Its_Bird_Oclock_Somewhere extends Redeem_Event_Base // Ref 268
+{
+    constructor(event)
+    {
+        super(event);
+        this.volume = 0.90;
+        this.event_icon = {"fg": "ega-white", "bg": "", "char": "v"};
+    }
+    
+    async play()  // Animate the event
+    {
+        $(this.event_target).animate({opacity: 1}, speed.event_fade, async value => {
+            sound.volume = this.volume;
+            let hour = $("#live-event").data("hour");
+            await play_sound(this.get_static_path() + "/bc-" + hour + ".mp3");
+            fade_out_event_card(this.event);
+        });
+    }
+}
+
+export class Event_Posture_Check extends Redeem_Event_Base // Ref 991
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 2000;
+        this.event_icon = {"fg": "ega-yellow", "bg": "ega-darkcyan-bg", "char": "☻"};
+        this.sound_filename = "pushing.wav";
+    }
+}
+
+export class Event_Streeeeeeeeeetch extends Redeem_Event_Base // Ref 159
+{
+    constructor(event)
+    {
+        super(event);
+        this.delay = 2000;
+        this.event_icon = {"fg": "ega-yellow", "bg": "ega-blue-bg", "char": "☺"};
+        this.sound_filename = "pushing.wav";
+    }
+}
+
+export class Event_Sub_Goal extends Redeem_Event_Base // Ref 3780
+{
+    constructor(event)
+    {
+        super(event);
+        this.volume = 0.5;
+        this.delay = 0;
+        this.event_icon = {"fg": "ega-red", "bg": "", "char": "♥"};
+        this.sound_filename = "newthingget-normalized.mp3";
+    }
+}
+
+export class Event_Use_The_3d_Talk_Engine extends Redeem_Event_Base // Ref 1033
+{
+    constructor(event)
+    {
+        super(event);
+        this.event_icon = {"fg": "ega-yellow", "bg": "ega-darkred-bg", "char": "☺"};
+        this.sound_filename = "3d-talk.wav";
+    }
+    
+    async play()  // Animate the event
+    {
+        $(this.event_target).animate({opacity: 1}, speed.event_fade, async value => {
+            sound.volume = this.volume;
+            let hour = $("#live-event").data("hour");
+            await play_sound(this.get_static_path() + "/" + this.sound_filename);
+            await delay(800);
+            await play_sound(this.get_static_path() + "/" + this.sound_filename);
+            await delay(1200);
+            fade_out_event_card(this.event);
+        });
+    }
+}
+
+export class Event_Yeaaaaahh extends Redeem_Event_Base // Ref 1046
+{
+    constructor(event)
+    {
+        super(event);
+        this.volume = 0.5;
+        this.delay = 200;
+        this.event_icon = {"fg": "ega-yellow", "bg": "ega-darkgreen-bg", "char": "☻"};
+        this.sound_filename = "yeah.wav";
+    }
+}
+
+export class Event_Zzt_Toilet_Flush extends Redeem_Event_Base // Ref 973
+{
+    constructor(event)
+    {
+        super(event);
+        this.volume = 0.4;
+        this.delay = 1000;
+        this.event_icon = {"fg": "ega-white", "bg": "", "char": "∩"};
+        this.sound_filename = "flush.mp3";
+    }
+}
+
 /* Old Style functions below */
-
-export async function beautiful_music(event)  /* Ref: #TBD */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        await delay(5000);
-        fade_out_event_card(event);
-    });
-}
-
-export async function bip_bo_beep(event)  /* Ref: #69 | #5930=Redwood */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.25;
-        for (var idx = 1; idx < 9; idx++)
-        {
-            await play_sound("/static/cdosstream/event/bip-bo-beep/f-" + (idx % 8 + 1)  + ".mp3");
-            await delay(random_int(1,8) * 50);
-        }
-        fade_out_event_card(event);
-    });
-}
-
-export async function channelcheer(event)  /* Ref: #260 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelcheer/bits-gem.wav");
-        await play_sound("/static/cdosstream/event/channelcheer/bits-gem.wav");
-        await play_sound("/static/cdosstream/event/channelcheer/bits-gem.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-
-export async function channelfollow(event)  /* Ref: #249 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelfollow/follow-transporter.wav");
-        await delay(3000);
-        fade_out_event_card(event);
-    });
-}
-
-
-export async function channelraid(event)  /* Ref: #1035 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelraid/raid-passage.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-
-export async function channelsubscribe(event)  /* Ref: #243 for basic sub, #1557 for receiving a gift sub */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        //update_sub_info();
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelsubscribe/sub-key.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-export async function channelsubscriptionmessage(event)  /* Ref: #1550 Resub */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        //update_sub_info();
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelsubscriptionmessage/sub-key.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-
-export async function channelsubscriptiongift(event)  /* Ref: 1565 / 1556 Gift Sub Sent */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        //update_sub_info();
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelsubscriptiongift/sub-key.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-export async function guide_the_raid(event)  /* Ref: #5598 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-		sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/channelraid/raid-passage.wav");
-        await delay(1600);
-        fade_out_event_card(event);
-    });
-}
-
-
-export async function hahaha(event)  /* Ref: #TBD */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/hahaha/hahaha.wav");
-        await delay(1000);
-        fade_out_event_card(event);
-    });
-}
-
-
-export async function hydrate(event)  /* Ref: #991 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/hydrate/hydrate-water.wav");
-        await delay(1000);
-        fade_out_event_card(event);
-    });
-}
-
-export async function its_bird_oclock_somewhere(event)  /* Ref: #268 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        let hour = $("#live-event").data("hour");
-        sound.volume = 0.90;
-        await play_sound("/static/cdosstream/event/its-bird-oclock-somewhere/bc-" + hour + ".mp3");
-        fade_out_event_card(event);
-    });
-    log("Exiting Birdclock");
-}
-
-
-export async function posture_check(event)  /* Ref: #252 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/posture-check/pushing.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-export async function streeeeeeeeeetch(event)  /* Ref: #159 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/streeeeeeeeeetch/pushing.wav");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
-export async function sub_goal(event)  /* Ref: #3780 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.5;
-        await play_sound("/static/cdosstream/event/sub-goal/newthingget-normalized.mp3");
-        fade_out_event_card(event);
-    });
-}
-
-export async function use_the_3d_talk_engine(event)  /* Ref: #1033 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.30;
-        await play_sound("/static/cdosstream/event/use-the-3d-talk-engine/3d-talk.wav");
-        await delay(800);
-        await play_sound("/static/cdosstream/event/use-the-3d-talk-engine/3d-talk.wav");
-        await delay(1200);
-        fade_out_event_card(event);
-    });
-}
-
-export async function yeaaaaahh(event)  /* Ref: #1046 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.50;
-        await play_sound("/static/cdosstream/event/yeaaaaahh/yeah.wav");
-        await delay(1600);
-        fade_out_event_card(event);
-    });
-}
-
-export async function zzt_toilet_flush(event)  /* Ref: #973 */
-{
-    $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
-        sound.volume = 0.40;
-        await play_sound("/static/cdosstream/event/zzt-toilet-flush/flush.mp3");
-        await delay(2000);
-        fade_out_event_card(event);
-    });
-}
-
 export async function random_scroll(event)  /* Ref: #2511 */
 {
     $("#live-event").animate({opacity: 1}, speed.event_fade, async function (){
